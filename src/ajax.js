2be54aac2c2af9591d0c972d6de59f9b95732837
Vyacheslav Lapin <vyacheslav_lapin@epam.com>
2019-02-28 11:04:07 +0300

    async/await added

--- src/ajax.js
+++ src/ajax.js
@@ -1,20 +1,34 @@
-new Promise((resolve, reject) => {
-  const xmlHttpRequest = new XMLHttpRequest();
-  xmlHttpRequest.onreadystatechange = () => {
-    if (xmlHttpRequest.readyState === 4) {
-      if (xmlHttpRequest.status === 200) {
-        resolve(xmlHttpRequest.text);
-      } else {
-        reject(xmlHttpRequest);
+function getPromise() {
+  return new Promise((resolve, reject) => {
+    const xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = () => {
+      if (xmlHttpRequest.readyState === 4) {
+        if (xmlHttpRequest.status === 200) {
+          resolve(xmlHttpRequest.text);
+        } else {
+          reject(xmlHttpRequest);
+        }
       }
-    }
-  };
-  xmlHttpRequest.open('GET', 'localhost:8080/1');
-  xmlHttpRequest.send(null);
-}).then((text) => {
-  const { id, price } = JSON.parse(text);
-  return id + price;
-}).then(value => console.log(value))
+    };
+    xmlHttpRequest.open('GET', 'localhost:8080/1');
+    xmlHttpRequest.send(null);
+  });
+}
+
+async function show() {
+  const text = await getPromise();
+  const {id, price} = JSON.parse(text);
+  console.log(id + price);
+}
+
+show()
   .catch(reason => console.log(reason.status));
 
-console.log('мама мыла раму');
+// getPromise()
+//   .then((text) => {
+//     const {id, price} = JSON.parse(text);
+//     return id + price;
+//   }).then(value => console.log(value))
+//   .catch(reason => console.log(reason.status));
+//
+// console.log('мама мыла раму');
