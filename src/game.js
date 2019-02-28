638a0afdf1c22a7959435cbdbbdc0c3cf8bff392
Vyacheslav Lapin <vyacheslav_lapin@epam.com>
2019-02-28 11:44:13 +0300

    lab №1 start

---
+++ src/game.js
@@ -0,0 +1,19 @@
+const BARREL_ITEMS = 7;
+const VELOCITY = 10;
+
+function rotateBarrel(index, force) {
+
+  // todo: realize it!
+  return undefined;
+}
+
+const force = Math.round(Math.random() * 100);
+const barrel1 = rotateBarrel(1, force);
+const barrel2 = rotateBarrel(2, force);
+const barrel3 = rotateBarrel(3, force);
+
+if (barrel1 === barrel2 && barrel2 === barrel3) {
+  console.log('Вы выиграли!');
+} else {
+  console.log('Вы проиграли!');
+}
